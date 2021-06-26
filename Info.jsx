ca5d0db5253d08a4044a7eded5d9f3e4d1b95b11
akshaya6121 <83197177+akshaya6121@users.noreply.github.com>
2021-06-26 07:14:09 -0700

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>7 Days, Certified, Free Javascript and Backend with Node.js</h1>
+      <p>
+        Here we bring our another free live BOOTCAMP with a hands-on project at
+        the end; Basic Back end Development with Javascript and Node.js- The
+        Foundation of Web Development .
+      </p>
+    </div>
+  );
+}
+
+export default Info;
